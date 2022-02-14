472f7eca67d76834d081e230b8dc00cd74b1513d
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2022-02-14 20:32:01 +0100

    Primera práctica completada

---
+++ src/main.jl
@@ -0,0 +1,50 @@
+using DelimitedFiles;
+using Statistics;
+
+maxMinNorm = function (v, min, max)
+	return (v .- min)./(max .- min);
+end;
+
+media0Norm = function (v, avg, std)
+	return (v .- avg)./std;
+end;
+
+dataset = readdlm("./BBDD/iris/iris.data", ',');
+f, c = size(dataset);
+
+inDS = dataset[:, 1:c-1];
+inDS[:, 1] .= 1; 
+outDS = dataset[:, c];
+categOutDS = unique(outDS);
+
+@assert length(categOutDS) > 2
+
+target = if length(categOutDS) == 2
+		categOutDS[1] .== outDS
+	else
+		map(x -> x .== categOutDS, outDS)
+	end;
+
+maxIn = maximum(inDS, dims=1);
+minIn = minimum(inDS, dims=1);
+avgIn = mean(inDS, dims=1);
+stdIn = std(inDS, dims=1);
+
+indexNullColumn = last.(Tuple.(findall((maxIn .== minIn) .* (stdIn .== 0))));
+
+if !isempty(indexNullColumn)
+	inDS = inDS[:, 1:end .!= indexNullColumn];
+end;
+
+normWithMaxMin = 0.75 .> maxMinNorm(avgIn, minIn, maxIn) .> 0.25;
+
+inputs = Array{Float32, 2}(undef, f, c-1);
+
+for i in 1:(c-1)
+	inputs[:, i] = if(normWithMaxMin[i])
+		maxMinNorm(inDS[:, i], minIn[i], maxIn[i])
+	else
+		media0Norm(inDS[:, i], avgIn[i], stdIn[i])
+	end;
+end;
+
