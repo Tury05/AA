43e851afe00d49925bc09a4ae333ed7ce479e1e7
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2022-02-14 22:18:55 +0100

    Interfaces de funciones para la practica 2

--- src/main.jl
+++ src/main.jl
@@ -33,6 +33,7 @@ stdIn = std(inDS, dims=1);
 indexNullColumn = last.(Tuple.(findall((maxIn .== minIn) .* (stdIn .== 0))));
 
 if !isempty(indexNullColumn)
+	c-=1;
 	inDS = inDS[:, 1:end .!= indexNullColumn];
 	maxIn = maxIn[:, 1:end .!= indexNullColumn];
 	minIn = minIn[:, 1:end .!= indexNullColumn];
@@ -52,3 +53,110 @@ for i in 1:(c-1)
 	end;
 end;
 
+#1
+oneHotEncoding = function (feature::AbstractArray{<:Any,1},
+		classes::AbstractArray{<:Any,1}=[])
+end;
+
+oneHotEncoding = function (feature::AbstractArray{<:Bool,1})
+end;
+
+
+#2
+calculateMinMaxNormalizationParameters = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+calculateZeroMeanNormalizationParameters = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+
+normalizeMinMax! = function (inputs::AbstractArray{<:Real,2},
+		minMax::NTuple{2, AbstractArray{<:Real,2}})
+	
+end;
+
+normalizeMinMax! = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+normalizeMinMax = function (inputs::AbstractArray{<:Real,2},
+		minMax::NTuple{2, AbstractArray{<:Real,2}}=())
+	
+end;
+
+normalizeMinMax = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+
+normalizeZeroMean! = function (inputs::AbstractArray{<:Real,2},
+		meanStd::NTuple{2, AbstractArray{<:Real,2}})
+	
+end;
+
+normalizeZeroMean! = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+normalizeZeroMean = function (inputs::AbstractArray{<:Real,2},
+		meanStd::NTuple{2, AbstractArray{<:Real,2}})
+	
+end;
+
+normalizeZeroMean = function (inputs::AbstractArray{<:Real,2})
+	
+end;
+
+
+#3 (dificultad media)
+classifyOutputs = function (outputs::AbstractArray{<:Real,2})
+	
+end;
+
+
+#4 (dificultad media)
+accuracy = function (target::AbstractArray{Bool,1},
+		outputs::AbstractArray{Bool,1})
+	
+end;
+
+accuracy = function (target::AbstractArray{Bool,2},
+		outputs::AbstractArray{Bool,2})
+	
+end;
+
+accuracy = function (target::AbstractArray{Bool,1},
+		outputs::AbstractArray{<:Real,1})
+	
+end;
+
+accuracy = function (target::AbstractArray{Bool,2},
+		outputs::AbstractArray{<:Real,2})
+	
+end;
+
+
+#5 (dificultad alta)
+classRNA = function (topology::AbstractArray{<:Int,1},
+		funActivacion::AbstractArray{Function,1},
+		dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}},
+		maxEpochs::Int=1000, minLoss::Real=0, learningRate::Real=0.01)
+	
+end;
+
+entrenarClassRNA = function (topology::AbstractArray{<:Int,1},
+		dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}},
+		maxEpochs::Int=1000, minLoss::Real=0, learningRate::Real=0.01)
+	
+end;
+
+entrenarClassRNA = function (topology::AbstractArray{<:Int,1},
+		dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}},
+		maxEpochs::Int=1000, minLoss::Real=0, learningRate::Real=0.01)
+	
+end;
+
+
+#6 (probar las funciones)
