55e18a18ef02db928540ac2436e7b977f79f3cbe
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2022-02-24 10:46:26 +0100

    Correcciones en la practica 3

--- src/main.jl
+++ src/main.jl
@@ -241,7 +241,7 @@ b = Array{Bool,1}(rand(8) .> 0.5)
 
 ann = entrenarClassRNA([2,2], (a,b), 10)
 
-# PRACTICA 2
+# PRACTICA 3
 
 # 1
 function holdOut(N::Int, P::Real)
@@ -314,4 +314,57 @@ function entrenarClassRNA(topology::AbstractArray{<:Int,1},
 	return entrenarClassRNA(topology, (first(dataset), reshape(last(dataset), :, 1)),
 		maxEpochs, minLoss, learningRate, (first(testset), reshape(last(testset), :, 1)),
 		(first(validset), reshape(last(validset), :, 1)), maxEpochsVal);
+end
+
+
+maxMinNorm = function (v, min, max)
+	return (v .- min)./(max .- min);
+end;
+
+media0Norm = function (v, avg, std)
+	return (v .- avg)./std;
+end;
+
+dataset = readdlm("./BBDD/iris/iris.data", ',');
+f, c = size(dataset);
+
+inDS = dataset[:, 1:c-1];
+inDS[:, 1] .= 1; 
+outDS = dataset[:, c];
+categOutDS = unique(outDS);
+
+@assert length(categOutDS) > 2
+
+target = if length(categOutDS) == 2
+		categOutDS[1] .== outDS
+	else
+		map(x -> x .== categOutDS, outDS)
+	end;
+
+maxIn = maximum(inDS, dims=1);
+minIn = minimum(inDS, dims=1);
+avgIn = mean(inDS, dims=1);
+stdIn = std(inDS, dims=1);
+
+indexNullColumn = last.(Tuple.(findall((maxIn .== minIn) .* (stdIn .== 0))));
+
+if !isempty(indexNullColumn)
+	c-=1;
+	inDS = inDS[:, 1:end .!= indexNullColumn];
+	maxIn = maxIn[:, 1:end .!= indexNullColumn];
+	minIn = minIn[:, 1:end .!= indexNullColumn];
+	avgIn = avgIn[:, 1:end .!= indexNullColumn];
+	stdIn = stdIn[:, 1:end .!= indexNullColumn];
+end;
+
+normWithMaxMin = 0.75 .> maxMinNorm(avgIn, minIn, maxIn) .> 0.25;
+
+inputs = Array{Float32, 2}(undef, f, c-1);
+
+for i in 1:(c-1)
+	inputs[:, i] = if(normWithMaxMin[i])
+		maxMinNorm(inDS[:, i], minIn[i], maxIn[i])
+	else
+		media0Norm(inDS[:, i], avgIn[i], stdIn[i])
+	end;
 end;
