fd11f2c8dce92a7e476067413a983ddd0ad4ef6b
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2022-03-12 14:03:45 +0100

    Añadidas funciones para cargar la BD de imagenes

--- src/main.jl
+++ src/main.jl
@@ -4,6 +4,50 @@ using Flux;
 using Flux.Losses;
 using Random;
 
+# Funciones para generar una BD con imagenes
+
+using JLD2
+using Images
+
+# Functions that allow the conversion from images to Float64 arrays
+imageToGrayArray(image:: Array{RGB{Normed{UInt8,8}},2}) = convert(Array{Float64,2}, gray.(Gray.(image)));
+imageToGrayArray(image::Array{RGBA{Normed{UInt8,8}},2}) = imageToGrayArray(RGB.(image));
+function imageToColorArray(image::Array{RGB{Normed{UInt8,8}},2})
+    matrix = Array{Float64, 3}(undef, size(image,1), size(image,2), 3)
+    matrix[:,:,1] = convert(Array{Float64,2}, red.(image));
+    matrix[:,:,2] = convert(Array{Float64,2}, green.(image));
+    matrix[:,:,3] = convert(Array{Float64,2}, blue.(image));
+    return matrix;
+end;
+imageToColorArray(image::Array{RGBA{Normed{UInt8,8}},2}) = imageToColorArray(RGB.(image));
+
+# Function to read all of the images in a folder and return them as 2 Float64 arrays: one with color components (3D array) and the other with grayscale components (2D array)
+function loadFolderImages(folderName::String)
+    isImageExtension(fileName::String) = any(uppercase(fileName[end-3:end]) .== [".JPG", ".PNG"]);
+    images = [];
+    for fileName in readdir(folderName)
+        if isImageExtension(fileName)
+            image = load(string(folderName, "/", fileName));
+            # Check that they are color images
+            @assert(isa(image, Array{RGBA{Normed{UInt8,8}},2}) || isa(image, Array{RGB{Normed{UInt8,8}},2}))
+            # Add the image to the vector of images
+            push!(images, image);
+        end;
+    end;
+    # Convert the images to arrays by broadcasting the conversion functions, and return the resulting vectors
+    return (imageToColorArray.(images), imageToGrayArray.(images));
+end;
+
+# Functions to load the dataset
+function loadTrainingDataset(positiveFolderName::String, negativeFolderName::String)
+    (positivesColor, positivesGray) = loadFolderImages(positiveFolderName);
+    (negativesColor, negativesGray) = loadFolderImages(negativeFolderName);
+    targets = [trues(length(positivesColor)); falses(length(negativesColor))];
+    return ([positivesColor; negativesColor], [positivesGray; negativesGray], targets);
+end;
+loadTestDataset() = ((colorMatrix,_) = loadFolderImages("test"); return colorMatrix; );
+
+
 maxMinNorm = function (v, min, max)
 	return (v .- min)./(max .- min);
 end;
